8db704b171531d4003dbb844ca0095516302c2b5
koiralakiran0 <kkoirala@uncc.edu>
2017-10-06 12:07:46 -0400

    started working on second Activity

--- Homework_3/app/src/main/java/com/example/koira/homework_3/GetAllQuestions.java
+++ Homework_3/app/src/main/java/com/example/koira/homework_3/GetAllQuestions.java
@@ -1,6 +1,7 @@
 package com.example.koira.homework_3;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
@@ -24,19 +26,39 @@ public class GetAllQuestions extends AsyncTask<String, Void, ArrayList<Question>
     }
 
     @Override
-    protected ArrayList<Question> doInBackground(String... strings) {
-        StringTokenizer stringTokenizer = null;
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
 
+    @Override
+    protected void onPostExecute(ArrayList<Question> questions) {
+        super.onPostExecute(questions);
+    }
+
+    @Override
+    protected ArrayList<Question> doInBackground(String... strings) {
+        BufferedReader reader = null;
         try {
             URL url = new URL(strings[0]);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            Log.d("demo", reader.readLine());
 
-            stringTokenizer = new StringTokenizer(reader.readLine());
+            String line;
+            while ((line = reader.readLine()) != null){
+                questions.add(new Question(line));
+            }
+
         } catch (MalformedURLException e){
             e.printStackTrace();
         } catch (IOException e){
             e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return questions;
