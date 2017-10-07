4f23f006a05f6f1df4c96fe8e84477a5fedd337a
koiralakiran0 <kkoirala@uncc.edu>
2017-10-07 19:41:02 -0400

    Reformat

---
+++ Modue6_Preparations/app/src/main/java/com/example/koira/modue6_preparations/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.koira.modue6_preparations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //JSONArray or JSONObject
+    }
+}
